9ba5a450387f64aede55336ac3e8400a1eebe848
zuu3 <ojh27467242@gmail.com>
2026-01-03 17:12:48 +0900

    feat(TC-2) :: App.tsx에 사이드바 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import { Routes, Route } from 'react-router-dom'
+import Sidebar from '@/components/ui/Sidebar'
+import styled from '@emotion/styled';
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<div>홈</div>} />
-        <Route path="*" element={<div>Not Found</div>} />
-      </Routes>
+    <AppContainer>
+      <Sidebar />
+      <MainContent>
+        <Routes>
+          <Route path="/" element={<div>홈</div>} />
+          <Route path="/supervision" element={<div>자습감독</div>} />
+          <Route path="/manage" element={<div>학생관리</div>} />
+          <Route path="/after-school" element={<div>방과후</div>} />
+          <Route path="/admin" element={<div>관리자</div>} />
+          <Route path="*" element={<div>Not Found</div>} />
+        </Routes>
+      </MainContent>
+    </AppContainer>
   )
 }
 
-export default App
+export default App;
+
+export const AppContainer = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+`
+
+export const MainContent = styled.main`
+  flex: 1;
+  overflow-y: auto;
+`
