68145657ee47ebcf8459bf7739884f7393482e55
zuu3 <ojh27467242@gmail.com>
2026-01-04 22:06:46 +0900

    feat(TC-2) :: create layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
-import Sidebar from '@/components/ui/Sidebar'
-import styled from '@emotion/styled';
+import MainLayout from '@/components/layout/MainLayout'
 
 function App() {
   return (
-    <AppContainer>
-      <Sidebar />
-      <MainContent>
-        <Routes>
-          <Route path="/" element={<div>홈</div>} />
-          <Route path="/supervision" element={<div>자습감독</div>} />
-          <Route path="/manage" element={<div>학생관리</div>} />
-          <Route path="/after-school" element={<div>방과후</div>} />
-          <Route path="/admin" element={<div>관리자</div>} />
-          <Route path="*" element={<div>Not Found</div>} />
-        </Routes>
-      </MainContent>
-    </AppContainer>
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<div>홈</div>} />
+        <Route path="/supervision" element={<div>자습감독</div>} />
+        <Route path="/manage" element={<div>학생관리</div>} />
+        <Route path="/after-school" element={<div>방과후</div>} />
+        <Route path="/admin" element={<div>관리자</div>} />
+      </Route>
+
+      <Route path="*" element={<div>Not Found</div>} />
+    </Routes>
   )
 }
 
-const AppContainer = styled.div`
-  display: flex;
-  width: 100vw;
-  height: 100vh;
-  overflow: hidden;
-`
-const MainContent = styled.main`
-  flex: 1;
-  overflow-y: auto;
-`
-
-export default App;
+export default App
---
+++ src/components/layout/MainLayout/style.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled'
+
+export const LayoutContainer = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+`
+
+export const MainContent = styled.main`
+  flex: 1;
+  overflow-y: auto;
+`
