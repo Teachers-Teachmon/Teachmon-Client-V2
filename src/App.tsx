468c1a7f1798db9c82ac9233feb7510a779ce3ea
whdkfk <joara080827@gmail.com>
2026-01-06 00:23:18 +0900

    feat/TC-4 :: App 코드백업

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Sidebar from '@/components/ui/Sidebar'
-import DateInput from '@/components/ui/input/date'
 import styled from '@emotion/styled';
 
 function App() {
-
   return (
     <AppContainer>
       <Sidebar />
       <MainContent>
         <Routes>
-          <Route path="/" element={
-            <div style={{ padding: '2rem' }}>
-              <h1>날짜 컴포넌트 테스트</h1>
-              
-              <div style={{ marginTop: '2rem' }}>
-                <DateInput label="시작일"/>
-              </div>
-
-              <div style={{ marginTop: '2rem' }}>
-                <DateInput />
-              </div>
-            </div>
-          } />
+          <Route path="/" element={<div>홈</div>} />
           <Route path="/supervision" element={<div>자습감독</div>} />
           <Route path="/manage" element={<div>학생관리</div>} />
           <Route path="/after-school" element={<div>방과후</div>} />
