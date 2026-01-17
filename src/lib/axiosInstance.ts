8fe52ddebf7add26c9027666a10d49149b4e4a69
dohun08 <bemik624@gmail.com>
2026-01-17 15:36:15 +0900

    feat/TC-33 :: 로딩 로직 추가

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,4 +1,5 @@
 import axios, { type AxiosError, type InternalAxiosRequestConfig } from 'axios';
+import { useLoadingStore } from '@/stores/useLoadingStore';
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
 
@@ -13,6 +14,10 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
     (config: InternalAxiosRequestConfig) => {
+        if (!config.skipLoading) {
+            useLoadingStore.getState().startLoading();
+        }
+
         const token = localStorage.getItem('accessToken');
         if (token && config.headers) {
             config.headers.Authorization = `Bearer ${token}`;
@@ -20,17 +25,24 @@ axiosInstance.interceptors.request.use(
         return config;
     },
     (error: AxiosError) => {
+        useLoadingStore.getState().stopLoading();
         return Promise.reject(error);
     }
 );
 
 axiosInstance.interceptors.response.use(
     (response) => {
+        if (!response.config.skipLoading) {
+            useLoadingStore.getState().stopLoading();
+        }
         return response;
     },
     async (error: AxiosError) => {
-        // TODO: 인증/인가 구현시 한번에 구현예정
+        if (!error.config?.skipLoading) {
+            useLoadingStore.getState().stopLoading();
+        }
 
+        // TODO: 인증/인가 구현시 한번에 구현예정
         return Promise.reject(error);
     }
 );
