7d806caf752d3491ab1e8a565ba6f5289f80fb0e
dohun08 <bemik624@gmail.com>
2026-01-19 13:30:28 +0900

    delete/TC-54 :: initializeDevice 제거

--- src/stores/useDeviceStore.ts
+++ src/stores/useDeviceStore.ts
@@ -4,21 +4,9 @@ import { breakpoints } from '@/styles/media';
 interface DeviceState {
   isMobile: boolean;
   setIsMobile: (isMobile: boolean) => void;
-  initializeDevice: () => void;
 }
 
 export const useDeviceStore = create<DeviceState>((set) => ({
-  isMobile: false,
+  isMobile: typeof window !== 'undefined' ? window.innerWidth <= breakpoints.mobile : false,
   setIsMobile: (isMobile: boolean) => set({ isMobile }),
-  initializeDevice: () => {
-    const checkMobile = () => {
-      set({ isMobile: window.innerWidth <= breakpoints.mobile });
-    };
-
-    checkMobile();
-    window.addEventListener('resize', checkMobile);
-
-    // cleanup은 컴포넌트에서 useEffect로 처리
-    return () => window.removeEventListener('resize', checkMobile);
-  },
 }));
