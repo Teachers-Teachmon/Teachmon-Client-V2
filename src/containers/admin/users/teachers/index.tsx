14bdac8ca85f431a2cb2c751b77d5eac2fc9127c
dohun08 <bemik624@gmail.com>
2026-02-10 21:35:00 +0900

    feat/TC-31 :: 임포트추가

--- src/containers/admin/users/teachers/index.tsx
+++ src/containers/admin/users/teachers/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useEffect } from 'react';
 import TableLayout from '@/components/layout/table';
 import Button from '@/components/ui/button';
 import { USER_ROLES } from '@/constants/admin';
