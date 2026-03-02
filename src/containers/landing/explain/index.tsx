2222e6daa4c45d53e974d0b0ef8bb18129ccb5f7
dohun08 <bemik624@gmail.com>
2026-03-03 00:39:23 +0900

    delete/TC-109 :: 어드민 영상 삭제

--- src/containers/landing/explain/index.tsx
+++ src/containers/landing/explain/index.tsx
@@ -2,58 +2,31 @@ import React, { useState, useRef } from 'react';
 import * as S from './style';
 
 const ExplainLanding: React.FC = () => {
-  const [isPlaying1, setIsPlaying1] = useState(false);
-  const [isLoading1, setIsLoading1] = useState(false);
-  const [isPlaying2, setIsPlaying2] = useState(false);
-  const [isLoading2, setIsLoading2] = useState(false);
-  const iframeRef1 = useRef<HTMLIFrameElement>(null);
-  const iframeRef2 = useRef<HTMLIFrameElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const iframeRef = useRef<HTMLIFrameElement>(null);
 
-  const handleOverlayClick1 = () => {
-    if (isLoading1 || !iframeRef1.current) return;
+  const handleOverlayClick = () => {
+    if (isLoading || !iframeRef.current) return;
 
-    setIsLoading1(true);
+    setIsLoading(true);
 
-    if (!isPlaying1) {
-      iframeRef1.current.contentWindow?.postMessage(
+    if (!isPlaying) {
+      iframeRef.current.contentWindow?.postMessage(
         '{"event":"command","func":"playVideo","args":""}',
         '*'
       );
-      setIsPlaying1(true);
+      setIsPlaying(true);
     } else {
-      iframeRef1.current.contentWindow?.postMessage(
+      iframeRef.current.contentWindow?.postMessage(
         '{"event":"command","func":"pauseVideo","args":""}',
         '*'
       );
-      setIsPlaying1(false);
+      setIsPlaying(false);
     }
 
     setTimeout(() => {
-      setIsLoading1(false);
-    }, 200);
-  };
-
-  const handleOverlayClick2 = () => {
-    if (isLoading2 || !iframeRef2.current) return;
-
-    setIsLoading2(true);
-
-    if (!isPlaying2) {
-      iframeRef2.current.contentWindow?.postMessage(
-        '{"event":"command","func":"playVideo","args":""}',
-        '*'
-      );
-      setIsPlaying2(true);
-    } else {
-      iframeRef2.current.contentWindow?.postMessage(
-        '{"event":"command","func":"pauseVideo","args":""}',
-        '*'
-      );
-      setIsPlaying2(false);
-    }
-
-    setTimeout(() => {
-      setIsLoading2(false);
+      setIsLoading(false);
     }, 200);
   };
 
@@ -64,7 +37,7 @@ const ExplainLanding: React.FC = () => {
         <S.VideosGrid>
           <S.VideoWrapper>
             <S.VideoIframe
-              ref={iframeRef1}
+              ref={iframeRef}
               src="https://www.youtube.com/embed/YM1tGbnY2wo?enablejsapi=1"
               title="YouTube video player 1"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -72,22 +45,8 @@ const ExplainLanding: React.FC = () => {
               allowFullScreen
             />
             <S.VideoOverlay 
-              onClick={handleOverlayClick1}
-              isLoading={isLoading1}
-            />
-          </S.VideoWrapper>
-          <S.VideoWrapper>
-            <S.VideoIframe
-              ref={iframeRef2}
-              src="https://www.youtube.com/embed/yFN13duHYSs?enablejsapi=1"
-              title="YouTube video player 2"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerPolicy="strict-origin-when-cross-origin"
-              allowFullScreen
-            />
-            <S.VideoOverlay 
-              onClick={handleOverlayClick2}
-              isLoading={isLoading2}
+              onClick={handleOverlayClick}
+              isLoading={isLoading}
             />
           </S.VideoWrapper>
         </S.VideosGrid>
